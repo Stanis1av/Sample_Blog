42c18c49d8d78bc1d5e58d50a6445da0d8521e95
Stanis1av <krasavin.stanislaus@gmail.com>
2020-11-08 18:05:22 +0300

    Implement articles#destroy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -34,6 +34,16 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def destroy
+    @article = Article.find(params[:id])
+
+    if @article.destroy
+      redirect_to @article
+    else
+      render action: 'destroy'
+    end
+  end
+
   private
 
   def article_params

054eae9ca1c25b4b736447fe72b9aff6042a82c4
Stanis1av <krasavin.stanislaus@gmail.com>
2020-11-08 20:49:03 +0300

    Add comments to post

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
 
   resource :contacts, only: [:new, :create], path_names: { :new => '' }
 
-  resources :articles
+  resources :articles do
+    resources :comments # Вложенный маршрут
+  end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

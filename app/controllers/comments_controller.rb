60c0fabbf688893cb5db60f281751917375340c5
Stanis1av <krasavin.stanislaus@gmail.com>
2020-11-12 12:29:21 +0300

    Saving in the database and displaying the username for each comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,14 @@
 class CommentsController < ApplicationController
-  before_action :authenticate_user!, :onle => [:create]
+  before_action :authenticate_user!, :only => [:create]
 
   def create
     @article = Article.find(params[:article_id])
-    @article.comments.create(comment_params)
+    @comment = @article.comments.new(comment_params)
+
+    @comment.author = current_user.username
+    @comment.save
+
+    # current_user.username
 
     redirect_to article_path(@article)
   end
@@ -11,7 +16,7 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    params.require(:comment).permit(:author, :body)
+    params.require(:comment).permit(:body) # :author, :body
   end
 
 end

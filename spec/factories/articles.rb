13aca7dfe171c841ce5316775d2298d14ac293e8
Stanis1av <krasavin.stanislaus@gmail.com>
2020-11-13 22:49:26 +0300

    Update article factory

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -2,5 +2,15 @@ FactoryBot.define do
   factory :article do
       title { 'Article title' }
       text { 'Article text' }
+
+      # Создаю фабрику с названием article_with_comments
+      # для создания статьи с несколькими комментариями
+      factory :article_with_comments do
+        # После создания article
+        after(:create) do |article, evaluator|
+          # создаю список из 3 комментариев
+          create_list(:comment, 3, article: article)
+        end
+      end
   end
 end

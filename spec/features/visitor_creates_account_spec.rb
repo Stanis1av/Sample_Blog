4b04e1504e69ab1ad86734df2144856e22fc619d
Stanis1av <krasavin.stanislaus@gmail.com>
2020-11-15 02:16:04 +0300

    add before(:each) to visitor_creates_account

--- spec/features/visitor_creates_account_spec.rb
+++ spec/features/visitor_creates_account_spec.rb
@@ -3,9 +3,12 @@ require 'rails_helper'
 require 'session_helper'
 
 feature "Account Creation" do
-  scenario "allows test to create account" do
-
+  before(:each) do
     sign_up
+  end
+
+  scenario "allows guest to create account" do
+
     expect(page).to have_content I18n.t('devise.registrations.signed_up')
   end
 end

041fec486661e00a771767bdcf8c931c663e7caf
Stanis1av <krasavin.stanislaus@gmail.com>
2020-11-15 02:16:34 +0300

    minor change #5

--- spec/session_helper.rb
+++ spec/session_helper.rb
@@ -1,8 +1,8 @@
 def sign_up
   visit new_user_registration_path
 
-    fill_in :user_email, :with => 'user@exaple.com'
-    fill_in :user_username, :with => 'Mike'
+    fill_in :user_email, :with => 'user4@exaple.com'
+    fill_in :user_username, :with => 'Mike4'
     fill_in :user_password, :with => '1234567'
     fill_in :user_password_confirmation, :with => '1234567'
 

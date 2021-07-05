89259f36f800be69a2601c5108cc41db8e146190
Stanis1av <krasavin.stanislaus@gmail.com>
2021-07-05 12:54:02 +0300

    Updating controllers  'Post', 'Comment' and 'Tag'

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,49 @@
 class CommentsController < ApplicationController
+  before_action :set_comment, only: [:edit, :update, :destroy]
+  before_action :set_post, only: [:create, :edit, :update]
+
+  def create
+    @comment = @post.comments.create(comment_params)
+    respond_to do |format|
+      if  @comment.save
+        format.html { redirect_to @post, notice: 'Comment was successfully created.' }
+        # NOTE COMMENT STEP 1: This will run the code in `app/views/comments/create.js.erb`.
+        format.js
+      else
+        format.html { render action: "new" }
+        # NOTE COMMENT STEP 1: This will run the code in `app/views/comments/create.js.erb`.
+        format.js
+      end
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @comment.update(comment_params)
+      redirect_to @comment.post, notice: "Comment updated."
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @comment.destroy
+    redirect_to @comment.post, notice: "Comment successfully deleted."
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:post_id])
+  end
+
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,64 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @comment = Comment.new(post: @post)
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.create(post_params)
+
+    @post.user_id = current_user.id
+
+    if @post.save
+      redirect_to @post, notice: "Post was successfully saved."
+    else
+      render action: "new"
+    end
+  end
+
+  def edit
+    if !is_the_owner
+      render @post
+    end
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to @post, notice: "Post was successfully updated."
+    else
+      render action: "edit"
+    end
+  end
+
+  def destroy
+    if is_the_owner
+      @post.destroy
+      redirect_to posts_path, notice: "Post was successfully deleted."
+    else
+      render actiom: "new"
+    end
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def is_the_owner
+    current_user.id == Post.find(params[:id]).user_id
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :body, :tag_list)
+  end
 end

643eab958408a79a5b335fe907eb1619bc7a21fd
Stanis1av <krasavin.stanislaus@gmail.com>
2021-07-05 12:20:58 +0300

    generation of migration 'Post', 'Comment', 'Tag', 'Tagging' and 'User'

--- db/migrate/20210704235053_create_taggings.rb
+++ db/migrate/20210704235053_create_taggings.rb
@@ -1,6 +1,8 @@
 class CreateTaggings < ActiveRecord::Migration[6.1]
   def change
     create_table :taggings do |t|
+      t.references :tag, null: false, foreign_key: true
+      t.references :post, null: false, foreign_key: true
 
       t.timestamps
     end

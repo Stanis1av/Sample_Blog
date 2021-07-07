787cda3450cd221f753b7d52aa1ab50821ba9fcb
Stanis1av <krasavin.stanislaus@gmail.com>
2021-07-07 21:28:06 +0300

    Various minor updates #2

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,11 +1,11 @@
 module PostsHelper
-    def article_tag_list
-        @article.tags.collect do |tag|
+    def post_tag_list
+        @post.tags.collect do |tag|
             tag.name
         end.join(", ")
     end
 
     def post_excerpt(post)
-        sanitize(truncate(post.body, length: 50)) if post && post.body.present?
+        sanitize(truncate(post.body, length: 150)) if post && post.body.present?
     end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# 50.times do
+#   user=User.new(
+#     username: Faker::Internet.user_name,
+#     email: Faker::Internet.email,
+#     password: Faker::Internet.password
+#   )
+#   user.save!
+# end
+
+# 50.times do
+#   tag=Tag.new(
+#     name: Faker::Lorem.words(number: rand(1..3)).join(" ")
+#   )
+#   tag.save!
+# end
+
+50.times do
+  post=Post.new(
+    title: Faker::Lorem.unique.sentence(word_count: rand(3..10)),
+    body: Faker::Lorem.unique.paragraph(sentence_count: rand(5..15)),
+    user_id: rand(User.first.id..User.last.id)
+  )
+  post.tags = Tag.order(Arel.sql('RANDOM()')).first(rand(2..8))
+  post.save!
+end
+
+500.times do
+  comment=Comment.new(
+    body: Faker::Lorem.unique.paragraph(sentence_count: rand(1..5)),
+    post_id: rand(Post.last.id..Post.first.id),
+    user_id: rand(User.first.id..User.last.id)
+  )
+  comment.save!
+end
